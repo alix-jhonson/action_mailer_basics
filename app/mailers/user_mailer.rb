ba74d8ff92e44f96c5d671fc3dc4200d6ad346dc
rana haseeb <alix@yahoo.com>
2022-07-13 14:16:46 +0500

    sending emails with dynamic delivery options

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,12 @@
 class UserMailer < ApplicationMailer
-  layout 'awesome' # use awesome.(html|text).erb as the layout
-  def welcome_email(user)
-    mail(to: user.email) do |format|
-      format.html { render layout: 'my_layout' }
-      format.text
-    end
+  def welcome_email(user, company)
+    @user = user
+    @url  = user_url(@user)
+    delivery_options = { user_name: company.smtp_user,
+                         password: company.smtp_password,
+                         address: company.smtp_host }
+    mail(to: @user.email,
+         subject: "Please see the Terms and Conditions attached",
+         delivery_method_options: delivery_options)
   end
 end
