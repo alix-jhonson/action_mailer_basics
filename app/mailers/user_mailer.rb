80678cac4e1650579e890ec07a9312b949bd01b7
rana haseeb <alix@yahoo.com>
2022-07-13 14:04:49 +0500

    action mailer layouts

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,30 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'notifications@example.com'
-
+  layout 'awesome' # use awesome.(html|text).erb as the layout
   def welcome_email(user)
-    @user = user
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: user.email) do |format|
+      format.html { render layout: 'my_layout' }
+      format.text
+    end
   end
-
-  def welcome
-  attachments.inline['image.jpg'] = File.read('/path/to/image.jpg')
-end
-  def welcome_email(user)
-  @user = user
-  email_with_name = %("#{@user.name}" <#{@user.email}>)
-  mail(to: email_with_name, subject: 'Welcome to My Awesome Site')
-end
-
-default from: 'notifications@example.com'
-
-  def welcome_email(user)
-    @user = user
-    @url  = 'http://example.com/login'
-    mail(to: @user.email,
-         subject: 'Welcome to My Awesome Site',
-         template_path: 'notifications',
-         template_name: 'another')
-  end
-
 end
