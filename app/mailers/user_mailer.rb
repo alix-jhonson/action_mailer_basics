368faf3d4cd591f6aed221ca92f53be10acd4daf
rana haseeb <alix@yahoo.com>
2022-07-13 13:47:52 +0500

    making_inline_attachments

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,8 @@ class UserMailer < ApplicationMailer
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
+
+  def welcome
+  attachments.inline['image.jpg'] = File.read('/path/to/image.jpg')
+end
 end
