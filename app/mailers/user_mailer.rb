2838603350a0f1be59c1047f3450ac7d548529dc
rana haseeb <alix@yahoo.com>
2022-07-13 15:39:47 +0500

    sending emails without template rendering

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,9 @@
 class UserMailer < ApplicationMailer
-  def welcome_email(user, company)
-    @user = user
-    @url  = user_url(@user)
-    delivery_options = { user_name: company.smtp_user,
-                         password: company.smtp_password,
-                         address: company.smtp_host }
-    mail(to: @user.email,
-         subject: "Please see the Terms and Conditions attached",
-         delivery_method_options: delivery_options)
+  def welcome_email(user, email_body)
+    mail(to: user.email,
+         body: email_body,
+         content_type: "text/html",
+         subject: "Already rendered!")
   end
+
 end
