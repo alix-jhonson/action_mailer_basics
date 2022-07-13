6bf0f259cb2b53be077e3a5b325677e726ddcaf8
rana haseeb <alix@yahoo.com>
2022-07-13 14:01:37 +0500

    mailer_views_method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,4 +16,15 @@ end
   mail(to: email_with_name, subject: 'Welcome to My Awesome Site')
 end
 
+default from: 'notifications@example.com'
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email,
+         subject: 'Welcome to My Awesome Site',
+         template_path: 'notifications',
+         template_name: 'another')
+  end
+
 end
